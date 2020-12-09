52c9f231b6c9ade937267401fe591cf454abd7a8
raihanulmcse@gmail.com <61967068+Showshad-UIA@users.noreply.github.com>
2020-12-09 23:19:03 +0800

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-book software</title>
+</head>
+<body>
+    <h1> E-Book Sharing software </h1>
+    <h2> It's an reliable application for any types of users  </h2>
+    <p> The application is very user friendly and easy to access from any platform.
+    There are many types of collection available here </p>
+</body>
+</html>
---
+++ list.blade.php
@@ -0,0 +1,33 @@
+<h1>The listed e-book  are:</h1>
+    <br>Have good day with books!!<br><br>
+    <h3>
+    Book Title : Adomo bakar <br>
+    Types : Chemistry <br>
+    ISBN : 0743223136 (ISO: ##00987) <br>
+    Orgin :Portugal
+    Number of pages : 1000pages
+    </h3><br>
+        <h3>
+        Book Title : Mahathir <br>
+            ISBN : 0373210515 (ISBN13: 9780373210510) <br>
+            Book Category : Politics <br>
+            Number of pages : 678 pages 
+            </h3><br>
+               <h3>
+                Book Title : Snowpiercer <br>
+                ISBN : 1582406723 (ISBN13: 9781582406725) <br>
+                Book Category : Thriller <br>
+                Number of pages : 354 pages
+                </h3><br>
+                    <h3>
+                    Book Title : Zombie Apocalypse <br>
+                    ISBN : 0545010225 (ISBN13: 9780545010221) <br>
+                    Book Category : Horror <br>
+                    Number of pages : 455 pages
+                    </h3><br>
+                        <h3>
+                        Book Title : Sarah Mcwilson <br>
+                        ISBN : 0439023483 (ISBN13: 9780439023481) <br>
+                        Book Category : Thriller <br>
+                        Number of pages : 666 pages
+                        </h3>
